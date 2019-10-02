ab108509452f7144d8aaf2909b32233e112c7e7e
Murtaza Ali <murtaza5152.ali@gmail.com>
2019-10-02 11:19:53 -0700

    Gave Device a housingtype attribute

--- Appliances_Class.py
+++ Appliances_Class.py
@@ -42,9 +42,10 @@ Ref12 = Refrigerant(650, 1, 0.005, 0.1, 0.3)   #...
 Ref13 = Refrigerant(1, 1, 0.005, 0.1, 0.3)
 
 class Device:
-    def __init__(self, name, fuel, ef, ef_cooler, C, outputTemp=125, s1, s2, Rval0, Rval1, Rval2, vintage, lt, IC, OM, hasRefrigerant=False, refrigerant=Refrigerant()):
+    def __init__(self, name, fuel, housetype=null, ef, ef_cooler, C, outputTemp=125, s1, s2, Rval0, Rval1, Rval2, vintage, lt, IC, OM, hasRefrigerant=False, refrigerant=Refrigerant()):
         self.name = name
         self.fuel = fuel
+        self.housetype = housetype #String indicating the type of house this device is for
         self.ef = ef  # Efficiency
         self.ef_cooler = ef_cooler
         self.C = C  # Climate zone number
