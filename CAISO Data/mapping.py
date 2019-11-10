13aed21f85e3fb5d70f3e8f0042904d5be8698e2
Murtaza Ali <murtaza5152.ali@gmail.com>
2019-11-10 10:54:29 -0800

    Read hourly averages into a dataframe structure and saved to CSV file improved

--- CAISO Data/mapping.py
+++ CAISO Data/mapping.py
@@ -29,4 +29,4 @@ for i in range(1, 12):
 
 month_table = pd.DataFrame(data_list, index = labels)
 print(month_table)
-month_table.to_csv('month_table.csv', index=False)
+month_table.to_csv('month_table.csv', index=True)
