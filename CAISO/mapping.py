efe461365ecfe43e815d04a7befd9c0e552ee166
Cidney Weng <cidneyweng@berkeley.edu>
2019-12-13 09:55:07 -0800

    daily hourly mapping and graph

--- CAISO/mapping.py
+++ CAISO/mapping.py
@@ -1,4 +1,31 @@
-import pandas as pd 
+import pandas as pd
+import matplotlib.pyplot as plt
+
+months = {1: "Jan",
+          2: "Feb",
+          3: "Mar",
+          4: "Apr",
+          5: "May",
+          6: "Jun",
+          7: "Jul",
+          8: "Aug",
+          9: "Sep",
+          10: "Oct",
+          11: "Nov",
+          12: "Dec"}
+
+month_lengths = {1: 31,
+                 2: 28,
+                 3: 31,
+                 4: 30,
+                 5: 31,
+                 6: 30,
+                 7: 31,
+                 8: 31,
+                 9: 30,
+                 10: 31,
+                 11: 30,
+                 12: 31}
 
 """for i in range(1, 12):
     demand = pd.read_csv('{}-1-19.csv'.format(i)).iloc[[2]]
@@ -42,19 +69,102 @@ for i in range(1, 12):
             total = 0
     energy_data_list.append(energy_storage)
 
+
+def get_monthly_emissions(month):
+    """
+    Takes in a month number (1->January... 12->December) and returns the dataframe corresponding to the
+    hourly emissions for each day of the month.
+    """
+    data_list = []
+    labels = []
+    for i in range(1, month_lengths[month] + 1):
+        storage = {}
+        hour, segment, total = 1, 1, 0
+        emissions = pd.read_csv('{}-{}-19-Emissions.csv'.format(month, i))
+        for i in range(1, emissions.shape[1]):
+            total += emissions.iloc[0, i]
+            segment += 1
+            if segment == 13:
+                segment = 1
+                storage[hour] = total / 12
+                hour += 1
+                total = 0
+        data_list.append(storage)
+        labels.append("{} {} Hourly Avg".format(months[month], i))
+    return pd.DataFrame(data_list, index=labels)
+
+
+def get_monthly_energy(month):
+    """
+    Takes in a month number (1->January... 12->December) and returns the dataframe corresponding to the
+    hourly energy usage for each day of the month.
+    """
+    energy_data_list = []
+    labels = []
+    for i in range(1, month_lengths[month] + 1):
+        storage = {}
+        hour, segment, total = 1, 1, 0
+        emissions = pd.read_csv('{}-{}-19.csv'.format(month, i))
+        for i in range(1, emissions.shape[1]):
+            total += emissions.iloc[0, i]
+            segment += 1
+            if segment == 13:
+                segment = 1
+                storage[hour] = total / 12
+                hour += 1
+                total = 0
+        energy_data_list.append(storage)
+        labels.append("{} {} Hourly Avg".format(months[month], i))
+    return pd.DataFrame(data_list, index=labels)
+
+
+def get_monthy_rates(month):
+    return get_monthly_emissions(month) / get_monthly_energy(month)
+
+
 month_table = pd.DataFrame(data_list, index=labels)
 energy_table = pd.DataFrame(energy_data_list, index=labels)
-# print("Emissions:")
-# print(month_table)
-# print("\nEnergy usage:")
-# print(energy_table)
 rates = month_table / energy_table
-# print("\nEmission rates in kg CO2/kWh:")
-# print(rates)
-jan = rates.iloc[0]
-print(jan)
-for item in jan.iteritems():
-    print(item[1])
+
+
+print("Emissions:")
+print(month_table)
+print("\nEnergy usage:")
+print(energy_table)
+print("\nEmission rates in kg CO2/kWh:")
+print(rates)
+"""
+
+print("Jan Emissions:")
+print(get_monthly_emissions(1))
+print("\nJan Energy usage:")
+print(get_monthly_energy(1))
+print("\nJan Emission rates in kg CO2/kWh:")
+print(get_monthy_rates(1))
+"""
+
+
+for i in range(1, 12):
+    month = months[i]
+    m = rates.iloc[i - 1]
+    m = list(m.iteritems())
+    m = [item[1] for item in m]
+    x = range(24)
+    plt.plot(x, m, label=month)
+plt.legend()
+plt.show()
+
+for i in range(1, 12):
+    month = months[i]
+    m = get_monthy_rates(i)
+    m = rates.iloc[i - 1]
+    m = list(m.iteritems())
+    m = [item[1] for item in m]
+    x = range(month_lengths[i])
+    plt.plot(x, m, label=month)
+plt.legend()
+plt.show()
+
 # month_table.to_csv('month_table.csv', index=True)
 # energy_table.to_csv('energy_table.csv', index=True)
 # rates.to_csv('rates.csv', index=True)
