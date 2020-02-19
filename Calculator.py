926390dad3a14c64bd7a026bcd5364005c2ecf1b
Matthew Ye <my1@berkeley.edu>
2020-02-19 15:16:57 -0800

    Outline for new pricing model

---
+++ Calculator.py
@@ -0,0 +1,29 @@
+# This will:
+# 1. Read LoadHourlyProfileData/HP_SH and HP_WH
+# 2. Store that data in a dataframe
+# 3. 
+import numpy as np
+import pandas as pd
+
+wh_data, sh_data
+def process_data():
+    global wh_data, sh_data
+
+
+def energy_usage_wh(month, climate_zone):
+
+def energy_usage_sh(month, climate_zone):
+
+# begin_date/end_date: use python datetime.date class
+# pricing scheme will have a flat_rate, 
+# tiers is a list of two-tuples: (threshold: price above that threshold)
+# tou is a list of 3-tuples: (begin_time, end_time, rate)
+# cost(begin_date, end_date)
+def cost_wh(data, begin_date, end_date, climate_zone, pricing_scheme):
+    # Take the appropriate column and data from climate_zone
+    # Create data_climate_zone
+    # PricingScheme.py
+    pricing_scheme.cost(data_climate_zone, begin_date, end_date)
+
+def cost_sh(data, begin_date, end_date, climate_zone, pricing_scheme)
+
