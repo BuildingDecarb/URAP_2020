c0054fcfa81aa4c10d63031f7fdadb99bebe340f
shubaragh <55463974+shubaragh@users.noreply.github.com>
2020-06-12 14:57:52 -0700

    Add files via upload

--- Bldg_Stats.py
+++ Bldg_Stats.py
@@ -2,25 +2,23 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.pyplot import *
-from Bldg_Read_test import *
+
 from matplotlib.pyplot import figure
+from Bldg_Aggregate import *
 
-name = "FRE"
-cnty = pd.read_csv(name + '2016_Master.csv')
+name = "TUL"   
+name1 = name
 
-#df = pd.read_csv("FRE2016.csv")
-#TUL = pd.read_csv("TUL2016_Dan_kelvin.csv")
-#Mer = pd.read_csv("MER2016.csv")
-
+filename = name + "2016_v1.csv"
 
 
+cnty = pd.read_csv(filename)
 building_types = cnty['Typef'].unique()
-avg_statistic_per_bldgtype(cnty, name, 'Typef', 'EEUIC', 'Building EEUIC (kWh/sqf)', 'Avg. Cooling Elec Intenstiy per Building Type in  ' + name )
-avg_statistic_per_bldgtype(cnty, name, 'Typef', 'NGEUISH', 'Building NG_SH (kWh/sqf)', 'Avg.NG Space heating Intenstiy per Building Type in  ' + name )
-avg_statistic_per_bldgtype(cnty, name, 'Typef', 'NGEUIWH', 'Building NG_WH (kWh/sqf)', 'Avg.NG Water heating Intenstiy per Building Type in  ' + name )
+avg_statistic_per_bldgtype(cnty, name, 'Typef', 'EEUIC', 'Building Elec Cooling (kWh/sqf)', 'Avg. Cooling Elec Intenstiy per Building Type in  ' + name1 + ' County')
+avg_statistic_per_bldgtype(cnty, name, 'Typef', 'NGEUISH', 'Building NG SH (kWh/sqf)', 'Avg.NG Space heating Intenstiy per Building Type in  ' + name1 +' County')
+avg_statistic_per_bldgtype(cnty, name, 'Typef', 'NGEUIWH', 'Building NG WH (kWh/sqf)', 'Avg.NG Water heating Intenstiy per Building Type in  ' + name1 + ' County')
 
-avg_statistic_per_bldgtype(cnty, name, 'Typef', 'BLDGAREA', 'Building Are (sqf)', 'Avg Building Area of Each Type in  ' + name + ' county')
-
+avg_statistic_per_bldgtype(cnty, name, 'Typef', 'BLDGAREA', 'Building Area (sqf)', 'Avg Building Area of Each Type in  ' + name1 + ' County')
 
 avg_bldgarea = avg_statistic_per_bldgtype_no_plot(cnty, 'Typef', 'BLDGAREA')[0]
 
@@ -55,12 +53,13 @@ for i in range(cnty['Typef'].unique().size):
 total_ngui = avg_ngeuish + avg_ngeuiwh
 print "total", total_ngui
 fig1 = plt.figure(figsize=(18, 6))
-plt.title("Total NG for Space & Water Heating in"+ name+ " County", fontsize= 15)
+plt.title("Total NG for Space & Water Heating in"+ name1 + " County", fontsize= 15)
 plt.xlabel("Building Types", fontsize = 10)
 plt.ylabel('Space & Water heating (kWh/sqf)', fontsize = 10)
 bar1 = plt.bar(cnty['Typef'].unique(), avg_ngeuish, color= 'r')
 bar2 = plt.bar(cnty['Typef'].unique(), avg_ngeuiwh, bottom = avg_ngeuish, color= 'orange')
-plt.legend((bar2,bar1), ('NG WH', 'NG SH'), fontsize = 10)
+#bar3 = plt.bar(cnty['Typef'].unique(), avg_eeuic, bottom= avg_ngeuish+avg_ngeuiwh, color = 'blue')
+plt.legend(( bar2,bar1), ('NG WH', 'NG SH'), fontsize = 10)
 ##bars = plt.bar(cnty['Typef'].unique(), avg_ngeuish + avg_ngeuiwh)
 ##i = 0
 ##for bar in bars:
@@ -71,27 +70,29 @@ plt.legend((bar2,bar1), ('NG WH', 'NG SH'), fontsize = 10)
 
 fig1.tight_layout()    
 
-fig1.savefig('plots/'+ name +  "_NG_Total_UEC" +  '_Aprilfig.png')
+fig1.savefig('plots/'+ name1 +  "_NG_Total_UEC" +  '_Aprilfig.png')
 
 # Conerting NG usage to electricity
 
-##EF_factor = 1.0   #0.8/4.0
-##
-##elec_sh = avg_ngeuish * EF_factor
-##elec_wh = avg_ngeuiwh * EF_factor
-##fig1 = plt.figure(figsize=(18, 6))
-##plt.title("Unit Electricity Intensity for cooling & heating")
-##plt.xlabel("Building Types")
-##plt.ylabel('Energy Intensity: Heating & Cooling (kWh/sqf)')
-##bar1 = plt.bar(cnty['Typef'].unique(), elec_sh, color= 'r')
-##bar2 = plt.bar(cnty['Typef'].unique(), elec_wh, bottom = elec_sh, color= 'orange')
-##bar3 = plt.bar(cnty['Typef'].unique(), avg_eeuic, bottom = elec_sh+elec_wh, color= 'blue')
-##
-##plt.legend((bar3,bar2,bar1), ('Coolng', 'Water Heating', ' Space Heating'))
-##
-##fig1.tight_layout()    
-##
-##fig1.savefig('plots/'+ name +  "Elec_heating_cooling" +  '_Aprilfig.png')
+EF1 =1 # 0.67/4.0
+EF2 = 1.0
+
+elec_sh = avg_ngeuish * EF1
+elec_wh = avg_ngeuiwh * EF1
+elec_sc = avg_eeuic * EF2
+fig1 = plt.figure(figsize=(18, 6))
+plt.title("Unit Electricity Intensity for cooling & heating")
+plt.xlabel("Building Types")
+plt.ylabel('Energy Intensity: Heating & Cooling (kWh/sqf)')
+bar1 = plt.bar(cnty['Typef'].unique(), elec_sh, color= 'r')
+bar2 = plt.bar(cnty['Typef'].unique(), elec_wh, bottom = elec_sh, color= 'orange')
+bar3 = plt.bar(cnty['Typef'].unique(), elec_sc, bottom = elec_sh+elec_wh, color= 'blue')
+
+plt.legend((bar3,bar2,bar1), ('Coolng', 'Water Heating', ' Space Heating'))
+
+fig1.tight_layout()    
+
+fig1.savefig('plots/'+ name +  "Elec_heating_cooling" +  '_June11fig.png')
 
 
 
